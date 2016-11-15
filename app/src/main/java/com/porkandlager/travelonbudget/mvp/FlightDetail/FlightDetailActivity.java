84aa424717169b665368bb25bdc59b0cf186e679
Batista Harahap <batista@bango29.com>
2016-11-15 21:36:32 +0700

    fixed layout not going fullscreen

--- app/src/main/java/com/porkandlager/travelonbudget/mvp/FlightDetail/FlightDetailActivity.java
+++ app/src/main/java/com/porkandlager/travelonbudget/mvp/FlightDetail/FlightDetailActivity.java
@@ -55,28 +55,6 @@ public class FlightDetailActivity extends Activity implements FlightDetailView {
 
     @Override
     public void makeFullscreen() {
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-            }
-            else {
-                mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-            }
-        }
-        else {
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-
         ActionBar actionBar = getActionBar();
         if(actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
